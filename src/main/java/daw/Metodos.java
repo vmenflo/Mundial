b29ca41b61d2fd97e87753d4501a19dcadfbaf3b
Victor <menaflores.victor@gmail.com>
2024-03-19 18:24:45 +0100

    +Casi terminado

--- src/main/java/daw/Metodos.java
+++ src/main/java/daw/Metodos.java
@@ -12,7 +12,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -22,48 +24,99 @@ import java.util.Scanner;
 public class Metodos {
 
     //Leer fichero
-    public static List<String> leerFichero(){
+    public static List<String> leerFichero() {
         String nombreFichero = "leer.txt";
         //Creamos una lista para almacenar la que nos devolvera ReadAllLines
         List<String> lista = new ArrayList<>();
         //Importante usar un try para controlar una posible excepcion
-        try{
+        try {
             lista = Files.readAllLines(Paths.get(nombreFichero));
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println("Error accediendo a " + nombreFichero);
         }
         lista.remove(0);
         return lista;
     }
-    
-    //Método para sumar
-    //Leer devolver matriz de una lista
-    public static int[][]devolver(List<String> lista){
-       int[][] matriz = new int[lista.size()-1][6];
-       lista.remove(0);
-       for(int i=0;i<lista.size()-1;i++){
-           String[] temporal = lista.get(i).split("\\s");
-           
-       }
-       return matriz;
-    }
-    
-    // Escribe un String a un fichero, sobreescribiendo si existe
-    public static void escribirString(String nombreFichero) {
-        Scanner teclado = new Scanner(System.in);
-        String entrada = " ";
 
-        do {
-            System.out.println("¿Qué deseas escribir?");
-            entrada = teclado.nextLine();
-            if (!entrada.equalsIgnoreCase("fin")) {
-                try {
-                    Files.write(Paths.get(nombreFichero), (entrada + "\n").getBytes(StandardCharsets.UTF_8),
-                            StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-                } catch (IOException ex) {
-                    System.out.println("Error creando el fichero");
-                }
+    //Leer devolver matriz de una lista
+    public static int[][] devolver(List<String> lista) {
+        int[][] matriz = new int[lista.size()][6];
+        for (int i = 0; i < matriz.length; i++) {
+            String[] temporal = lista.get(i).split("\\s");
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = Integer.parseInt(temporal[j]);
             }
-        } while (!entrada.equalsIgnoreCase("fin"));
+        }
+        return matriz;
     }
+
+    //Método para ver matriz
+    public static void verMatriz(int[][] matriz) {
+        for (int[] matriz1 : matriz) {
+            for (int j = 0; j < matriz1.length; j++) {
+                System.out.print(matriz1[j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    //Vamos a pasar de una matriz de int a una lista
+    public static int[] listaSumatoria(int[][] matriz) {
+        //Creamos el listado de Map
+        int[] listado = new int[matriz.length];
+        int acumulador = 0;
+        //Obtenemos los datos
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 1; j < matriz[i].length; j++) {
+                acumulador += matriz[i][j];
+            }
+            listado[i] = matriz[i][0] - acumulador;
+            acumulador = 0;
+        }
+        return listado;
+    }
+
+    //Método para escribir un fichero
+    public static void generarFicheroADevolver(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            try {
+                Files.write(Paths.get("resultado.txt"), (array[i] + "\n").getBytes(StandardCharsets.UTF_8),
+                        StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            } catch (IOException ex) {
+                System.out.println("Error creando el fichero");
+            }
+        }
+    }
+    
+    //Método para leer
+    public static void leer() {
+        String idFichero="resultado.txt";
+        // Variables para guardar los datos que se van leyendo
+        String[] tokens;
+        String linea;
+
+        System.out.println("Leyendo el fichero: " + idFichero);
+
+        // Inicialización del flujo "datosFichero" en función del archivo llamado "idFichero"
+        // Estructura try-with-resources. Permite cerrar los recursos una vez finalizadas
+        // las operaciones con el archivo
+        try (Scanner datosFichero = new Scanner(new File(idFichero), "UTF-8")) {
+            // hasNextLine devuelve true mientras haya líneas por leer
+            while (datosFichero.hasNextLine()) {
+                // Guarda la línea completa en un String
+                linea = datosFichero.nextLine();
+                // Se guarda en el array de String cada elemento de la
+                // línea en función del carácter separador de campos del fichero CSV
+                tokens = linea.split("\n");
+                for (String string : tokens) {
+                    System.out.print(string + "\t");
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- src/main/java/daw/Mundial.java
+++ src/main/java/daw/Mundial.java
@@ -4,6 +4,8 @@
 
 package daw;
 
+import java.util.List;
+
 /**
  *
  * @author victor
@@ -11,8 +13,27 @@ package daw;
 public class Mundial {
 
     public static void main(String[] args) {
-        //Creamos el fichero
+        //Obtenemos la lista del fichero
+        List<String> lista = Metodos.leerFichero();
+        lista.forEach(System.out::println);
        
+        //De la lista obtenemos una matriz
+        int[][] matriz = Metodos.devolver(lista);
+        //La mostramos
+        System.out.println("Mostramos la matriz de int generada"
+                + "por el método a partir de la lista ");
+        Metodos.verMatriz(matriz);
         
+        //Devolvemos de esa matriz un int[] con los resultados
+       int[]listado = Metodos.listaSumatoria(matriz);
+        System.out.println("Mostramos el array de int para ver que esta bien");
+        for (int i : listado) {
+            System.out.println(i); 
+        }
+        
+        //Generamos un fichero llamado resultado.txt con la informacion
+        Metodos.generarFicheroADevolver(listado);
+        //Lo enseñamos para comprobarlo
+        Metodos.leer();
     }
 }
